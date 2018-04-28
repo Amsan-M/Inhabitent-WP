dd5e1298254aaa83c718fcbeb38204bd885c5ff4
Nasma <nasma.mlamali@icloud.com>
2018-04-28 16:53:19 -0700

    first commit

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,12 +11,21 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
+				<p> info@inhabitent.com <p>
+				<i class="fas fa-phone"></i>
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
+		 <!--user icon in two different styles-->
+
+		 <i class="fab fa-facebook-square"></i>
+		 <i class="fab fa-twitter-square"></i>
+		 <i class="fab fa-google-plus-square"></i>
+  <!--brand icon-->
+
 
 	</body>
 </html>
