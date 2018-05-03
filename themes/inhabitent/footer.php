e64edd44e8afeb6a56127b1adc153ceac988e6aa
Nasma <nasma.mlamali@icloud.com>
2018-05-03 13:57:05 -0700

    css

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,21 +10,48 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+
+
 				<div class="site-info">
-				<p> info@inhabitent.com <p>
-				<i class="fas fa-phone"></i>
+
+
+					<div class= "contact"> 
+						<p>CONTACT INFO<p>
+						<p>info@inhabitent.com <p>
+						<p>	<i class="fas fa-phone"></i> 778-456-7891<p>
+						<p>	<i class="fab fa-facebook-square"></i>
+	        	            <i class="fab fa-twitter-square"></i>
+		                    <i class="fab fa-google-plus-square"></i> <p>
+					</div>    <!-- .contact-->
+					
+
+				    <div class= "buisness-hours"> 
+				      <p>BUISNESS HOURS<p> 
+				      <p>Monday-Friday: 9am to 5pm<p>
+				      <p>Saturday:10am to 2pm<p>
+				      <p>Sunday: closed<p> 
+					</div>   <!-- .buisness-hours -->
+
+<div class= "footer-image">
+	
+</div>
+				  <!--brand icon-->
+				
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				
+				
 				</div><!-- .site-info -->
+			
+			
 			</footer><!-- #colophon -->
+
+
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
 		 <!--user icon in two different styles-->
 
-		 <i class="fab fa-facebook-square"></i>
-		 <i class="fab fa-twitter-square"></i>
-		 <i class="fab fa-google-plus-square"></i>
-  <!--brand icon-->
+		 
 
 
 	</body>
--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -20,3 +20,27 @@ function red_starter_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'red_starter_body_classes' );
+
+
+// inhabitent logo instead of wordpress
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+        background-image: url(<?php echo get_stylesheet_directory_uri();?>/assets/images/logos/inhabitent-logo-tent.svg);
+		height:80px;
+		width:320px;
+		background-size: 320px 65px;
+		background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+// change logo url 
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
