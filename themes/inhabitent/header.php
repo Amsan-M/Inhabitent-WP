ef1d22282f2e3fe8ebdcf0f3543e37597ecc2baf
Nasma <nasma.mlamali@icloud.com>
2018-05-07 16:27:35 -0700

    font-face problem fixed, next add the fonts where appropriate

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -13,7 +13,8 @@
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-		<!-- <link rel="stylesheet" type="text/css" href="style.min.css.css"> -->
+		
+		
 	<?php wp_head(); ?>
 	</head>
 
