8b5a44f69f7572c069ac745af00a9a37c4e8a72a
Nasma <nasma.mlamali@icloud.com>
2018-05-21 13:32:48 -0700

    javascript toggle function

--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -1,15 +1,16 @@
 
 
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+<form role="search" id= "search-toggle" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
 		<label>
 			<input type="search" class="search-field" placeholder="TYPE AND HIT ENTER" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 		<!-- <button class="search-submit"> -->
 			<span class="icon-search" aria-hidden="true">
-				<i class="fa fa-search"></i>
+				
 			</span>
 			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
 		<!-- </button> -->
 	</fieldset>
 </form>
+<button id="toggle-searchbar" onClick=toggleSearch();>logo</button>
