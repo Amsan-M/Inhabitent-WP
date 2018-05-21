1f419c31372ec186516ed0db7ff94f8e5af09b83
Nasma <nasma.mlamali@icloud.com>
2018-05-21 14:33:13 -0700

    focus added to toggle function on search bar

--- themes/inhabitent/js/header-functions.js
+++ themes/inhabitent/js/header-functions.js
@@ -1,4 +1,20 @@
 jQuery( document ).ready(function () {
-    alert('hey1');
+    // alert('hey1');
+
+
     
 });
+
+function toggleSearch(){
+    var searchBar = document.getElementById('search-toggle');
+    if (searchBar.style.display === 'none'|| searchBar.style.display=='') {
+        searchBar.style.display = 'block';
+        document.getElementById("input-focus").focus();
+    } else {
+        searchBar.style.display = 'none';
+    }
+}
+// toggleSearch();
+document.getElementById("toogle-searchbar").onclick = function(){
+    toggleSearch();
+};
--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -3,7 +3,7 @@
 <form role="search" id= "search-toggle" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
 		<label>
-			<input type="search" class="search-field" placeholder="TYPE AND HIT ENTER" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+			<input type="search" id= "input-focus" class="search-field" placeholder="TYPE AND HIT ENTER" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 		<!-- <button class="search-submit"> -->
 			<span class="icon-search" aria-hidden="true">
